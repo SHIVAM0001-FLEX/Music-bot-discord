6ab0f3212fa02325f267a4b69fdc57b1cd98288d
RayenMichael <81325534+RayenMichael@users.noreply.github.com>
2021-03-25 11:50:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();//Loading .env
+const fs = require("fs");
+const { Collection, Client } = require("discord.js");
+
+const client = new Client();//Making a discord bot client
+client.commands = new Collection();//Making client.commands as a Discord.js Collection
+client.queue = new Map()
+client.config = {
+prefix: process.env.PREFIX
+}
+client.config = {
+prefix: process.env.PREFIX,
+SOUNDCLOUD: process.env.SOUNDCLOUD_CLIENT_ID
+}
+var http = require('http');
+
+http.createServer(function (req, res) {
+res.write("The Server is Running!");
+res.end();
+}).listen(8080);
+client.on('ready', () => { ('Discord.Command')
+
+
+})
+
+
+
+
+//Loading Events
+fs.readdir(__dirname + "/events/", (err, files) => {
+if (err) return console.error(err);
+files.forEach((file) => {
+const event = require(__dirname + `/events/${file}`);
+let eventName = file.split(".")[0];
+client.on(eventName, event.bind(null, client));
+console.log("Loading Event: "+eventName)
+});
+});
+
+//Loading Commands
+fs.readdir("./commands/", (err, files) => {
+if (err) return console.error(err);
+files.forEach((file) => {
+if (!file.endsWith(".js")) return;
+let props = require(`./commands/${file}`);
+let commandName = file.split(".")[0];
+client.commands.set(commandName, props);('JavaCloser')
+console.log("Loading Command: "+commandName)
+});
+});
+
+//Logging in to discord (*.Commander*)
+client.login(process.env.TOKEN)
